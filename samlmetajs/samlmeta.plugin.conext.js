86648235b551a5d6889a18bdd48b91cbb34e9e8e
Niels van Dijk <niels.vandijk@surfnet.nl>
2013-07-01 16:38:49 +0200

    Added capabilities for parsing Conext form

--- samlmetajs/samlmeta.plugin.conext.js
+++ samlmetajs/samlmeta.plugin.conext.js
@@ -34,7 +34,21 @@
 				e.preventDefault();
 				$(e.target).closest('fieldset').remove();
 			});
-		}
+		},
+        "toTextarea": function(entitydescriptor) {
+            console.log("toTextarea");
+
+            $(entitydescriptor.conextData).each(function (index, element) {
+                console.log($(element));
+
+                /*cArr = new Array();
+                cArr["contract"] = new Array();
+                $(element).find('input:checked').each(function(index2, element2) {
+                    cArr["contract"].push($(element2).attr('name'));
+                });
+                newConextData.push(cArr);*/
+            });
+        }
 	};
 
 	SAMLmetaJS.plugins.conext = {
@@ -55,7 +69,7 @@
 			var conextHTML ="";
 			
 			// Contracts and Licesing
-			conextHTML += '<fieldset><legend>Contract & Licensing</legend>' +
+			conextHTML += '<fieldset id="contract"><legend>Contract & Licensing</legend>' +
 					'<div><p>Any connection to the SURFconext platform is subject to a <a href="http://www.surfconext.nl/contractexample.html">SURFconext contract</a>, stating security, data protection and privacy regulations.' +
 					'<p>Institutions that are <a target="top" href="http://www.surf.nl/nl/oversurf/instellingen/Pages/Default.aspx">members of SURF</a> can connect (non-commercial) Services for their own users, as well as share services with other institutions. Connecting these services requires no additional contracts if your institution is already acting as an Identity Provider.</br>'+
 					'<p>Commercial Service require a licence agreement in addition to the SURFconext contract. Service providers will have to contact <a href="mailto:services@surfconext.nl">the Services team of SURFmarket</a> first.' +
@@ -70,34 +84,34 @@
 					'</fieldset>';
 			
 			// Propose		
-			conextHTML += '<fieldset><legend>Purpose of the Service</legend>' +
+			conextHTML += '<fieldset id="purpose"><legend>Purpose of the Service</legend>' +
 					'<div><p>Providing the propose of the service helps us determine the feasability of connecting your service to SURFconext. In addition we will use the description of the service to weigh the release of attributes (user characteristics) you may request.</p>'+
 					'Please describe the purpose of the service</div>' +
 					'<div style="float: left; width: 70%" title="SURFmarket"><textarea name="purpose" id="purpose" style="width: 100%"></textarea>' +
 					'</fieldset>';					
 					
 			// Previous Install
-			conextHTML += '<fieldset><legend>Previous Install</legend>' +
+			conextHTML += '<fieldset id="experience"><legend>Previous Install</legend>' +
 					'<div><p>An indication of level of expertise you or your organization has with setting up an SAML based service provider helps us better estimate the amount of support you may require.</p>' + 
 					'Is there any experience with setting up and maintaining a SAML2 based service provider?</div>' +
 					'<div style="float: left; width: 70%" title="SURFmarket">' +
-					'<input type="checkbox" id="previousInstall" name="previousInstall" />' +
-					'<label for="previousInstall">Yes, I or my organisation has previously installed and/or maintains a SAML2 based Service Provider</label>' +					
+					'<input type="checkbox" id="yes_previous_install" name="yes_previous_install" />' +
+					'<label for="yes_previous_install">Yes, I or my organisation has previously installed and/or maintains a SAML2 based Service Provider</label>' +
 					'</br>' +					
-					'<input type="checkbox" id="previousInstall" name="previousInstall" />' +
-					'<label for="previousInstall">No, I/We are new to this whole SAML2 based Service Provider thing</label>' +					
+					'<input type="checkbox" id="no_previous_install" name="no_previous_install" />' +
+					'<label for="no_previous_install">No, I/We are new to this whole SAML2 based Service Provider thing</label>' +
 					'</fieldset>';
 
 			// Current or Launching Customers
-			conextHTML += '<fieldset><legend>Current or Launching Customers</legend>' +
+			conextHTML += '<fieldset id="customers"><legend>Current or Launching Customers</legend>' +
 					'<div><p>Indicating current or launching customers among the SURF member institutions helps us determine which institutions we may need to contact to help you get set up..</p>'+					
 					'Do you have any current or launching customers for your service? If so, please list these below</div>' +
-					'<div style="float: left; width: 70%" title="SURFmarket"><textarea name="customersList" id="customersList" style="width: 100%"></textarea>' +
+					'<div style="float: left; width: 70%" title="SURFmarket"><textarea name="customers" id="customers" style="width: 100%"></textarea>' +
 					'</div>' +					
 					'</fieldset>';
 			
 			// Test or Production		
-			conextHTML += '<fieldset><legend>Test or Production</legend>' +
+			conextHTML += '<fieldset id="state"><legend>Test or Production</legend>' +
 					'<div><p>A production connection to the platform can only be realised after a succesful test connection has been made. '+
 					'For testing porposes, SURFnet offers a <a href="">Do-It-Yourself Platform</a> that allows you to test your technical setup in a fashion very similar to a production connection</br>' + 
 					'Please note that a production connection cannot be established without a signed SURFconext contract and optionally a license agreement</p>' +
@@ -111,11 +125,11 @@
 					'</fieldset>';	
 					
 			// Deadlines			
-			conextHTML += '<fieldset><legend>Planning & Deadlines</legend>' +
+			conextHTML += '<fieldset id="planning"><legend>Planning & Deadlines</legend>' +
 					'<div><p>As soon as <a href="">technical</a> and <a href="">contractual</a> requiments are met, SURFnet will need between 3 to 5 days to operationalize the connection.</br>'+
 					'For managing expectations and our planning, please provide us with information when you would like to have the connection you are currently requesting operational.</p>'+ 					
 					'When do you want your service to be connected?</div>' +
-					'<div style="float: left; width: 70%" title="Deadline"><textarea name="Deadline" id="Deadline" style="width: 100%"></textarea>' +
+					'<div style="float: left; width: 70%" title="Deadline"><textarea name="planning" id="planning" style="width: 100%"></textarea>' +
 					'</div>' +					
 					'</fieldset>';						
 
@@ -131,13 +145,103 @@
 
 		fromXML: function () {
 			// Nothing to Do
+
+            console.log("Conext fromXML");
 		},
 
 		toXML: function () {
 			// Nothing to Do
+
+            console.log("Conext toXML");
+            //console.log(entitydescriptor);
+
+            var newConextData = new Array();
+            if (!entitydescriptor.conextData) {
+                entitydescriptor.conextData = new Array();
+            }
+
+            /* Available Fieldsets
+             contract
+             purpose
+             experience
+             customers
+             state
+             planning
+             */
+
+            // contract
+            $('div#conext fieldset#contract').each(function (index, element) {
+                cArr = new Array();
+                cArr["contract"] = new Array();
+                    $(element).find('input:checked').each(function(index2, element2) {
+                        cArr["contract"].push($(element2).attr('name'));
+                });
+                newConextData.push(cArr);
+            });
+
+            // purpose
+            $('div#conext fieldset#purpose').each(function (index, element) {
+                pArr = new Array();
+                pArr["purpose"] = $(element).find('textarea#purpose').eq(0).attr('value');
+                newConextData.push(pArr);
+            });
+
+            // experience
+            $('div#conext fieldset#experience').each(function (index, element) {
+                eArr = new Array();
+                eArr["experience"] = new Array();
+                $(element).find('input:checked').each(function(index2, element2) {
+                    eArr["experience"].push($(element2).attr('name'));
+                });
+                newConextData.push(eArr);
+            });
+
+            // customers
+            $('div#conext fieldset#customers').each(function (index, element) {
+                //console.log($(element));
+
+                cuArr = new Array();
+                cuArr["customers"] = $(element).find('textarea#customers').eq(0).attr('value');
+                newConextData.push(cuArr);
+            });
+
+            // state
+            $('div#conext fieldset#state').each(function (index, element) {
+                sArr = new Array();
+                sArr["state"] = new Array();
+                $(element).find('input:checked').each(function(index2, element2) {
+                    sArr["state"].push($(element2).attr('name'));
+                });
+                newConextData.push(sArr);
+            });
+
+            // planning
+            $('div#conext fieldset#planning').each(function (index, element) {
+
+                //console.log($(element));
+
+                plArr = new Array();
+                plArr["planning"] = $(element).find('textarea#planning').eq(0).attr('value');
+                newConextData.push(plArr);
+            });
+
+            //console.log(newConextData);
+
+            entitydescriptor.conextData.push(newConextData);
+
+ //           console.log($("#conextdata"));
+ //           $("#conextdata")[0].attr('value')="test123";
+
+            var textarea = document.getElementById("conextdata");
+            textarea.value = newConextData.join("\n");
+
+            console.log($("#conextdata"));
 		},
 		
 		validate: function () {
+
+            console.log("Conext validate function");
+
 			return true;
 		}
 	};
