7135d880ca51cc63ea238fe9735cc21ca3ff7dc2
Niels van Dijk <niels.vandijk@surfnet.nl>
2013-08-30 15:18:42 +0200

    Added php page to handle form content being submitted

---
+++ processSPform.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: niels
+ * Date: 8/30/13
+ * Time: 3:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var_dump($_POST["metadata"]);
+
+print("<hr>");
+
+var_dump($_POST["conextdata"]);
+
+
+
+
+?>
