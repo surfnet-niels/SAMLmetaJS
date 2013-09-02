9a0d53327ec3df5a28294ef3aa6d2faeb9dbd41c
Niels van Dijk <niels.vandijk@surfnet.nl>
2013-09-02 16:45:32 +0200

    Added authN simplesamp integration

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <php
-require_once('../../lib/_autoload.php');
+require_once('../../../simplesamlphp/lib/_autoload.php');
 
 $as = new SimpleSAML_Auth_Simple('default-sp');
 $as->requireAuth();
-
+;
 ?>
 
 
