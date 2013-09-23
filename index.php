1ce7b608b83f60dffede95ee3a4a659494abc4d9
surfnet_niels <@pdV,G+\B(8)/s00|([Ck)i*>
2013-09-23 18:58:50 +0200

    Updated descriptions

--- index.php
+++ index.php
@@ -111,13 +111,24 @@ $as->requireAuth();
 
     <h3>Purpose</h3>
     <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tempus non orci a convallis. Maecenas non tortor vitae odio volutpat blandit. Pellentesque volutpat cursus risus in sollicitudin. Praesent nec nisl eget enim tempus gravida. Phasellus in luctus libero, eget sollicitudin purus. Maecenas id velit dolor. Sed vel erat convallis, tempus urna sit amet, mattis velit.
+        When you want to use SURFconext for the SAML2 based authentication to your service, some organisational and technical information needs to be exchanged. The way this is done is through the exchange of a SAML2 metadata. This form helps you to provide SURFconext with correct and complete metadata. This will ensure the connection process is as efficient as it can be.
     </p>
 
     <h3>Using this form</h3>
     <p>
-        If you already have a SAML2 metadata file, you can open the last tab of the below form ("metadata") and past the XML into it.
-        The form will automagically process your data and detect ny missing information after you refresh the page (press F5). For more information see the Service provider registration wiki page.
+        Most SAML2 SP software will generate SAML2 metadata for you. Unfortunately, the SAML2 metadata standards allows many options and very few are mandatory. SURFconext expects some optional elements to be present (like organisational information) and doen't allow some others.
+        <br>
+        This form's primairy focus is on checking your generated SAML2 metadata for SURFconext compatibility. You can open the last tab of the below
+        form ("metadata") and past the metadata XML into it. The form will automatically process your data and detect any missing information after you refresh the page. Update all the missing information and refresh again. If there are no more warnings then the metadata is ready to send to the SURFconext administrators. This can be done by going to the
+        "metadata" tab, scrolling to the bottom and pressing the "send" button.
+        <br>
+        If you do not have a SAML2 metadata file, you can generate it manually here. You need go through all the tabs and enter the missing information
+        and send it in. This method is not advisable as you need to have a good understanding of SAML2 to enter the technical information correctly.
+        <br>
+        Note that this form cannot be saved while working on it. If you leave the SP form page your work will be lost. This means that it is advisable
+        to first collect all the necessary information before completing and sending it.
+        <br>
+        For more information see the <a href="https://wiki.surfnetlabs.nl/display/surfconextdev/Documentation+for+Service+Providers">SURFconext service provider wiki page</a>.
     </p>
 	
 	<form action="https://support.surfconext.nl/spform/processSPform.php" method="post" name=samlmetajsform>
